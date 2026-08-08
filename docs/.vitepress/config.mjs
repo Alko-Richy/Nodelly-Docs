f8d28fe60831e0658df7e0326e1904487060bc4b
Alko.Richy <alko.richy@my.com>
2026-08-08 21:44:01 +0300

    Update 1

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -14,11 +14,23 @@ export default {
       {
         text: 'Documentation',
         items: [
+          { text: 'Quick Start', link: '/quick-start' },
           { text: 'Installation', link: '/installation' },
           { text: 'Sorting', link: '/sorting' },
           { text: 'Checker', link: '/checker' },
           { text: 'Validator', link: '/validator' },
-          { text: 'Managers', link: '/managers' }
+          {
+            text: 'Managers',
+            link: '/managers',
+            items: [
+              { text: 'String Manager', link: '/managers/string-manager' },
+              { text: 'Array Manager', link: '/managers/array-manager' },
+              { text: 'Randomizer Manager', link: '/managers/randomizer-manager' },
+              { text: 'File Manager', link: '/managers/file-manager' },
+              { text: 'Config Manager', link: '/managers/config-manager' }
+            ]
+          }
+          { text: 'Changelog', link: '/changelog' }
         ]
       }
     ]
