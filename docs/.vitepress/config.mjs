48eb9fa5b3360e408a53fa39e19251f53f8a7a1b
Alko.Richy <alko.richy@my.com>
2026-08-08 13:01:17 +0300

    Initial docs

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,25 @@
+export default {
+  title: 'Nodelly',
+  description: 'A lightweight Node.js utility library',
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Installation', link: '/installation' },
+      { text: 'API', link: '/sorting' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Documentation',
+        items: [
+          { text: 'Installation', link: '/installation' },
+          { text: 'Sorting', link: '/sorting' },
+          { text: 'Checker', link: '/checker' },
+          { text: 'Validator', link: '/validator' },
+          { text: 'Managers', link: '/managers' }
+        ]
+      }
+    ]
+  }
+};
